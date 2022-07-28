e2a1e1d2f67a51e4bd2e6c248438fbf3bd286ff5
gss <2436748193@qq.com>
2022-07-28 22:56:31 +0800

    【脚手架搭建】:【Vite、vue-router】

---
+++ src/views/home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home">
+        123
+        <p>456</p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    console.log("输出", import.meta.env);
+</script>
+
+<style lang="scss" scoped>
+.home{
+    color: red;
+    p{
+        color: gold;
+    }
+}
+</style>
